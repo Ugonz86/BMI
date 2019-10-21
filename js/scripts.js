8525284c83cccbfb13fd51c01df415529433f8c0
Jordan Safford and Uriel Gonzalez <uriel@>
2019-10-21 11:56:07 -0700

    create BMI calculator

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var divide = function(weight, heightSqr) {
+  return (weight / heightSqr) * 703;
+};
+
+var weight = parseInt(prompt("Enter your weight in pounds:"));
+var heightSqr = parseInt(prompt("Enter  your height in inches squared:"));
+var result = divide(weight, heightSqr);
+alert(result);
